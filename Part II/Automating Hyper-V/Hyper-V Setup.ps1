b12159fbe0ab695631a6f159613d8090f4077282
Adam Bertram <adbertram@gmail.com>
2018-01-15 14:30:18 -0600

    Updates

--- Part II/Automating Hyper-V/Hyper-V Setup.ps1
+++ Part II/Automating Hyper-V/Hyper-V Setup.ps1
@@ -1,2 +1,6 @@
 ## Run this on the server you want to be a Hyper-V host
 Install-WindowsFeature -Name Hyper-V -IncludeManagementTools -Restart 
+
+'C:\PowerLab', 'C:\PowerLab\VMs', 'C:\PowerLab\VHDs' | ForEach-Object {
+	$null = mkdir -Path $_
+}
