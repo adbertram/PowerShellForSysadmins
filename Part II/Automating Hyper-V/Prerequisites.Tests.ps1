f48e9922d77f6703d6873359cf50ef059e24a0a2
Adam Bertram <adbertram@gmail.com>
2018-01-14 18:42:11 -0600

    Updates

--- Part II/Automating Hyper-V/Prerequisites.Tests.ps1
+++ Part II/Automating Hyper-V/Prerequisites.Tests.ps1
@@ -1,11 +1,12 @@
 describe 'Automating Hyper-V Chapter Prerequisites' {
 
 	it 'Hyper-V host server should have the Hyper-V Windows feature installed' {
-		Get-WindowsFeature -Name 'Hyper-V' | should not benullorEmpty
-		$hyperVFeature.Installed | should be $true
+		$feature = Get-WindowsFeature -Name 'Hyper-V'
+		$feature | should not benullorEmpty
+		$feature.Installed | should be $true
 	}
 
 	it 'Hyper-V host server is Windows Server 2016' {
-		(Get-CimInstance -Class Win32_OperatingSystem).Caption | should be 'Microsoft Windows Server 2016'
+		(Get-CimInstance -Class Win32_OperatingSystem).Caption | should belike 'Microsoft Windows Server 2016'
 	}
 }
