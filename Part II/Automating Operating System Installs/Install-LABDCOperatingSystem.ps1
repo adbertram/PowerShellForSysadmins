e62dacacf2d4f21517eccd1f6252df51eab75c83
Adam Bertram <adbertram@gmail.com>
2018-01-15 11:42:01 -0600

    Updates

---
+++ Part II/Automating Operating System Installs/Install-LABDCOperatingSystem.ps1
@@ -0,0 +1,24 @@
+$isoFilePath = 'C:\PowerLab\ISOs\en_windows_server_2016_x64_dvd_9718492.iso'
+$answerFilePath = 'C:\Automate-The-Boring-Stuff-With-PowerShell\Part II\Automating Operating System Installs\LABDC.xml'
+
+$convertParams = @{
+	SourcePath        = $isoFilePath
+	SizeBytes         = 40GB
+	Edition           = 'ServerStandardCore'
+	VHDFormat         = 'VHDX'
+	VHDPath           = 'C:\PowerLab\VHDs\LABDC.vhdx'
+	VHDType           = 'Dynamic'
+	VHDPartitionStyle = 'GPT'
+	UnattendPath      = $answerFilePath
+}
+
+. 'C:\Automate-The-Boring-Stuff-With-PowerShell\Part II\Automating Operating System Installs\Convert-WindowsImage.ps1'
+
+Convert-WindowsImage @convertParams
+
+$vm = Get-Vm -Name 'LABDC'
+$vm | Add-VMHardDiskDrive -Path 'C:\PowerLab\VHDs\LABDC.vhdx'
+$bootOrder = ($vm | Get-VMFirmware).Bootorder
+if ($bootOrder[0].BootType -ne 'Drive') {
+	$vm | Set-VMFirmware -FirstBootDevice $vm.HardDrives[0]
+}
--- Part II/Automating Operating System Installs/Install-OperatingSystem.ps1
+++ Part II/Automating Operating System Installs/Install-OperatingSystem.ps1
@@ -8,13 +8,21 @@ param
 $scriptPath = 'C:\Automate-The-Boring-Stuff-With-PowerShell\Part II\Automating Operating System Installs'
 $answerFile = Get-Item -Path "$scriptPath\LABDC.xml"
 
-$cvtParams = @{
-	IsoFilePath    = 'C:\PowerLab\ISOs\en_windows_server_2016_x64_dvd_9718492.iso'
-	VhdPath        = 'C:\PowerLab\VHDs\LABDC.vhdx'
-	AnswerFilePath = $AnswerFile.FullName
+## Dot-source the script. Since the script has a function, doing this will make the function inside the script available
+. "$scriptPath\Convert-WindowsImage.ps1"
+$convertParams = @{
+	SourcePath        = 'C:\PowerLab\ISOs\en_windows_server_2016_x64_dvd_9718492.iso'
+	SizeBytes         = 40GB
+	Edition           = 'ServerStandardCore'
+	VHDFormat         = 'VHDX'
+	VHDPath           = 'C:\PowerLab\VHDs\LABDC.vhdx'
+	VHDType           = 'Dynamic'
+	VHDPartitionStyle = 'GPT'
+	UnattendPath      = $AnswerFile.FullName
 }
 
-$vhd = & "$scriptPath\ConvertTo-VirtualDisk.ps1" @cvtParams
+Convert-WindowsImage @convertParams
+Get-Vhd -Path $VHDPath
 
 $vm = Get-Vm -Name $vm.Name
 $vm | Add-VMHardDiskDrive -Path $vhd.Path
