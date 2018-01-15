f8f34e6a556557522b3dd4ba06f1eb770bf2b7a2
Adam Bertram <adbertram@gmail.com>
2018-01-15 12:21:48 -0600

    Updates

--- Part II/Automating Operating System Installs/Install-OperatingSystem.ps1
+++ Part II/Automating Operating System Installs/Install-OperatingSystem.ps1
@@ -2,30 +2,86 @@ param
 (
 	[Parameter(Mandatory)]
 	[ValidateNotNullOrEmpty()]
-	[object]$Vm
+	[string]$VmName,
+
+	[Parameter(Mandatory)]
+	[ValidateNotNullOrEmpty()]
+	[ValidateScript({ Test-Path -Path $_ -PathType Leaf })]
+	[string]$UnattendFilePath,
+
+	[Parameter()]
+	[ValidateNotNullOrEmpty()]
+	[string]$OperatingSystem = 'Server 2016',
+
+	[Parameter()]
+	[ValidateNotNullOrEmpty()]
+	[ValidateSet('ServerStandardCore')]
+	[string]$OperatingSystemEdition = 'ServerStandardCore',
+
+	[Parameter()]
+	[ValidateNotNullOrEmpty()]
+	[string]$DiskSize = 40GB,
+
+	[Parameter()]
+	[ValidateNotNullOrEmpty()]
+	[string]$VhdFormat = 'VHDX',
+
+	[Parameter()]
+	[ValidateNotNullOrEmpty()]
+	[string]$VhdType = 'Dynamic',
+
+	[Parameter()]
+	[ValidateNotNullOrEmpty()]
+	[string]$VhdPartitionStyle = 'GPT',
+
+	[Parameter()]
+	[ValidateNotNullOrEmpty()]
+	[string]$VhdPath
 )
 
+## Define the base folder
 $scriptPath = 'C:\Automate-The-Boring-Stuff-With-PowerShell\Part II\Automating Operating System Installs'
-$answerFile = Get-Item -Path "$scriptPath\LABDC.xml"
+
+## Assuming we have multiple unattend XML files in this folder named <VMName>.xml
+$answerFile = Get-Item -Path "$scriptPath\$VMName.xml"
 
 ## Dot-source the script. Since the script has a function, doing this will make the function inside the script available
 . "$scriptPath\Convert-WindowsImage.ps1"
+
+## Here is where we could add mulitple OS support picking the right ISO depending on the OS version chosen
+$baseIsoPath = 'C:\PowerLab\ISOs'
+switch ($OperatingSystem) {
+	'Server 2016' {
+		$isoFilePath = "$baseIsoPath\en_windows_server_2016_x64_dvd_9718492.iso"
+	}
+	default {
+		throw "Unrecognized input: [$_]"
+	}
+}
+
+## Assuming all VHDs live here and are called <VMName>.vhdx
+$vhdBasePath = 'C:\PowerLab\VHDs'
+
 $convertParams = @{
-	SourcePath        = 'C:\PowerLab\ISOs\en_windows_server_2016_x64_dvd_9718492.iso'
-	SizeBytes         = 40GB
-	Edition           = 'ServerStandardCore'
-	VHDFormat         = 'VHDX'
-	VHDPath           = 'C:\PowerLab\VHDs\LABDC.vhdx'
-	VHDType           = 'Dynamic'
+	SourcePath        = $isoFilePath
+	SizeBytes         = $DiskSize
+	Edition           = $OperatingSystemEdition
+	VHDFormat         = $VhdFormat
+	VHDPath           = 
+	VHDType           = $VhdType
 	VHDPartitionStyle = 'GPT'
-	UnattendPath      = $AnswerFile.FullName
+	UnattendPath      = $UnattendFilePath
+}
+if ($PSBoundParameters.ContainsKey('VhdPath')) {
+	$convertParams.VHDPath = $VhdPath
+} else {
+	$convertParams.VHDPath = "$vhdBasePath\$VMName.vhdx"
 }
 
 Convert-WindowsImage @convertParams
-Get-Vhd -Path $VHDPath
 
-$vm = Get-Vm -Name $vm.Name
-$vm | Add-VMHardDiskDrive -Path $vhd.Path
+$vm = Get-Vm -Name $VmName
+$vm | Add-VMHardDiskDrive -Path $convertParams.VHDPath
 $bootOrder = ($vm | Get-VMFirmware).Bootorder
 if ($bootOrder[0].BootType -ne 'Drive') {
 	$vm | Set-VMFirmware -FirstBootDevice $vm.HardDrives[0]
