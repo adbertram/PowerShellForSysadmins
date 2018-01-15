9cdeb97e2b1ddcb55c7ebf3528ee7566d2aa71c2
Adam Bertram <adbertram@gmail.com>
2018-01-15 12:53:01 -0600

    updates

--- Part II/Automating Operating System Installs/Automating Operating System Installs.Tests.ps1
+++ Part II/Automating Operating System Installs/Automating Operating System Installs.Tests.ps1
@@ -1,3 +1,41 @@
 describe 'Automating Operating System Installs' {
 
+	context 'Virtual Disk' {
+		
+		$expectedVhdPath = 'C:\PowerLab\VHDs\LABDC.vhdx'
+
+		it 'created a VHDX called LABDC in the expected location' {
+			$expectedVhdPath | should exist
+		}
+
+		it 'attached the virtual disk to the expected VM' {
+			Get-VM -Name LABDC | Get-VMHardDiskDrive | Where-Object { $_.Path -eq $expectedVhdPath } | should not benullorEmpty
+		}
+
+		it 'creates the expected VHDX format' {
+			(Get-VHD -Path $expectedVhdPath).VhdFormat | should be 'VHDX'
+		}
+
+		it 'creates the expected VHDX partition style' {
+
+		}
+
+		it 'creates the expected VHDX type' {
+			(Get-VHD -Path $expectedVhdPath).Type | should be 'Dynamic'
+		}
+
+		it 'creates the VHDDX of the expected size' {
+			(Get-VHD -Path $expectedVhdPath).Size / 1GB | should be 40
+		}
+	}
+
+	context 'Operating System' {
+		it 'deploys the expected Windows version' {
+
+		}
+
+		it 'deploys the expected Windows edition' {
+
+		}
+	}
 }
