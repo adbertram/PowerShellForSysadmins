db0e71b192658df6b462bb5b6e5e05665d7ad852
Adam Bertram <adbertram@gmail.com>
2018-01-21 14:00:24 -0600

    AD updates

---
+++ Part II/Creating an Active Directory Forest/New-ActiveDirectoryTestObject.ps1
@@ -0,0 +1,38 @@
+param(
+	[Parameter(Mandatory)]
+	[string]$SpreadsheetPath
+)
+
+$users = Import-Excel -Path $SpreadsheetPath -WorksheetName Users
+$groups = Import-Excel -Path $SpreadsheetPath -WorksheetName Groups
+
+$cred = Import-CliXml -Path 'C:\PowerLab\DomainCredential.xml'
+$dcSession = New-PSSession -VMName LABDC -Credential $cred
+
+$scriptBlock = {
+	foreach ($group in $using:groups) {
+		if (-not (Get-AdOrganizationalUnit -Filter "Name -eq '$($group.OUName)'")) {
+			New-AdOrganizationalUnit -Name $group.OUName
+			Write-Host $group.OUName
+		}
+		if (-not (Get-AdGroup -Filter "Name -eq '$($group.GroupName)'")) {
+			New-AdGroup -Name $group.GroupName -GroupScope $group.Type -Path "OU=$($group.OUName),DC=powerlab,DC=local"
+			write-host $group.GroupName
+		}
+	}
+
+	foreach ($user in $using:users) {
+		if (-not (Get-AdOrganizationalUnit -Filter "Name -eq '$($user.OUName)'")) {
+			New-AdOrganizationalUnit -Name $user.OUName
+		}
+		if (-not (Get-AdUser -Filter "Name -eq '$($user.UserName)'")) {
+			New-AdUser -Name $user.UserName -Path "OU=$($user.OUName),DC=powerlab,DC=local"
+		}
+		if ($user.UserName -notin (Get-AdGroupMember -Identity $user.MemberOf).Name) {
+			Add-AdGroupMember -Identity $group.MemberOf -Members $user.UserName
+		}
+	}
+}
+
+Invoke-Command -Session $dcSession -ScriptBlock $scriptBlock
+$dcSession | Remove-PSSession
