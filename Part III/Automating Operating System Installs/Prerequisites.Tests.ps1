e008f44cd35c64cf78033719cc59efcfbfa00ebf
Adam Bertram <adbertram@gmail.com>
2024-08-27 12:18:15 -0500

    Update Prerequisites.Tests.ps1

--- Part III/Automating Operating System Installs/Prerequisites.Tests.ps1
+++ Part III/Automating Operating System Installs/Prerequisites.Tests.ps1
@@ -2,7 +2,7 @@ describe 'Automating Operating System Install Prerequisites' {
 
 	$isosPath = 'C:\PowerLab\ISOs'
 	$windowsIsoPath = "$isosPath\en_windows_server_2016_x64_dvd_9718492.iso"
-	$convertWimImageScriptPath = 'C:\PowerShellForSysAdmins\Part II\Automating Operating System Installs\Convert-WindowsImage.ps1'
+	$convertWimImageScriptPath = 'C:\PowerShellForSysAdmins\Part III\Automating Operating System Installs\Convert-WindowsImage.ps1'
 	$vmName = 'LABDC'
 
 	it 'has an ISOs folder created' {
